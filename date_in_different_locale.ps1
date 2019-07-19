4095c6d120e70fbb8545d4794759b4d6fa901e11
AM <anvar.minulin@gmail.com>
2019-07-19 22:27:31 +0300

    PowerShell working with date in another locale

---
+++ date_in_different_locale.ps1
@@ -0,0 +1,6 @@
+﻿#Get date as correct datateime object if you have different locale with gathered date from source
+$date = "01/12/2019 23:00:00"
+$new_Date = [datetime]::ParseExact($date, "dd/MM/yyyy HH:mm:ss", [CultureInfo]("en-US"))
+$new_Date
+$new_Date_in_db_format = "{0:yyyy-MM-dd HH:mm:ss}" -f $new_Date
+$new_Date_in_db_format
