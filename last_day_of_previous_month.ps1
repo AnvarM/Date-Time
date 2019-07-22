843e96009c83e569daf18154ffbe8ea71b71d5dd
AM <anvar.minulin@gmail.com>
2019-07-22 21:03:23 +0300

    Get last day of previous month in powershell

---
+++ last_day_of_previous_month.ps1
@@ -0,0 +1,4 @@
+﻿$PriorMonth = (Get-Date).AddMonths(-1)
+$LastDay = [System.DateTime]::DaysInMonth($PriorMonth.Year, $PriorMonth.Month)
+$lastDayOfPriorMonth = [datetime]"$($PriorMonth.Month), $LastDay, $($PriorMonth.Year), 23:59:59.999"
+$lastDayOfPriorMonth
