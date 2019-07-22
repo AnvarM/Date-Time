2d678e34b73cb2372e254d2b1663d806b5f759f4
AM <anvar.minulin@gmail.com>
2019-07-22 21:11:27 +0300

    Add second way, how to get last day of previous month

--- last_day_of_previous_month.ps1
+++ last_day_of_previous_month.ps1
@@ -1,4 +1,10 @@
-﻿$PriorMonth = (Get-Date).AddMonths(-1)
+﻿#First way
+$PriorMonth = (Get-Date).AddMonths(-1)
 $LastDay = [System.DateTime]::DaysInMonth($PriorMonth.Year, $PriorMonth.Month)
-$lastDayOfPriorMonth = [datetime]"$($PriorMonth.Month), $LastDay, $($PriorMonth.Year), 23:59:59.999"
-$lastDayOfPriorMonth
+$LastDayOfPriorMonth = [datetime]"$($PriorMonth.Month), $LastDay, $($PriorMonth.Year), 23:59:59.999"
+$LastDayOfPriorMonth
+
+#Second way
+$FirstDayOfCurrentMonth = Get-Date -Day "1" -Hour "23" -Minute "59" -Second "59"
+$LastDayOfPriorMonth = $FirstDayOfCurrentMonth.AddDays(-1)
+$LastDayOfPriorMonth
